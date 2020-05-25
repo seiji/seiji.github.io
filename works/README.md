90e4a3da559a11c3ffb3dc9efdbe9d52ff2463ea
seiji <toyama.seiji@gmail.com>
2020-05-25 16:31:45 +0900

    Add aws-sso

---
+++ works/README.md
@@ -0,0 +1,1 @@
+npm init deck sample && cd sample
---
+++ works/aws-sso/theme.js
@@ -0,0 +1,101 @@
+import colors from './components/colors'
+
+export default {
+  colors,
+  googleFont: 'https://fonts.googleapis.com/css?family=Source+Code+Pro',
+  fonts: {
+    body: '"Source Code Pro", monospace',
+    heading: '"Source Code Pro", monospace',
+    monospace: '"Source Code Pro", monospace',
+  },
+  fontWeights: {
+    body: 400,
+    heading: 600,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  text: {
+    heading: {
+      fontFamily: 'heading',
+      fontWeight: 'heading',
+      lineHeight: 'heading',
+    },
+  },
+  styles: {
+    Slide: {
+      justifyContent: 'flex-start',
+      margin: '0 auto',
+      overflow: 'visible',
+      padding: '6rem 6rem',
+      position: 'inherit',
+      width: '80vw',
+      '& [name="wrapper"]': {
+        padding: '0 1em'
+      }
+    },
+    Header: {
+      color: 'sub',
+    },
+    h3: {
+      width: '74vw',
+      padding: '0rem',
+      margin: '2rem 0rem 1rem 0rem',
+      fontWeight: 200,
+    },
+    h4: {
+      width: '74vw',
+      padding: '0rem',
+      margin: '2rem 0rem 2rem 2rem',
+      fontWeight: 100,
+    },
+    a: {
+      color: colors.link || colors.text,
+      fontSize: '1.6rem'
+    },
+    blockquote: {
+      width: '70vw',
+      borderLeft: "10px solid #ddd",
+      color: "#999",
+      fontSize: '0.8em',
+      padding: "0.2em",
+      whiteSpace: "pre-line",
+      margin: '1rem 0'
+    },
+    p: {
+      width: '74vw',
+      margin: '1rem'
+    },
+    pre: {
+      width: '70vw',
+      color: 'background',
+      fontSize: '0.7em',
+      bg: 'text',
+      padding: '0.4em',
+      margin: '0em'
+    },
+    ul: {
+      width: '70vw',
+      padding: '0 1rem',
+      margin: '1rem 0rem 0rem 0rem',
+    },
+    li: {
+      fontSize: '1.2em',
+      padding: '1rem 0'
+    },
+    img: {
+      width: '100%',
+      maxHeight: '80vh',
+      display: 'block',
+      margin: '0',
+      padding: '0'
+    },
+    code: {
+      bg: 'text',
+      color: 'background',
+      lineHeight: '1.4em',
+      padding: '0'
+    },
+  }
+}
